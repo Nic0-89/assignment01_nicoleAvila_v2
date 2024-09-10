8895ee2bf653fe9bce33311490641db981a890c4
Nicole Avila <avila.nicky89@gmail.com>
2024-09-10 17:27:21 +0200

    second version trying to create POM

---
+++ actions/commonActions.ts
@@ -0,0 +1,30 @@
+import { Page } from 'playwright';
+
+export default class CommonActions {
+  private page: Page;
+
+  constructor(page: Page) {
+    this.page = page;
+  }
+
+  async navigate(url: string): Promise<void> {
+    // await this.page.pause(); // Uncomment this line if you need to pause the page
+    await this.page.goto(url);
+  }
+
+  async click(selector: string): Promise<void> {
+    await this.page.click(selector);
+  }
+
+  async fill(selector: string, text: string): Promise<void> {
+    await this.page.fill(selector, text);
+  }
+
+  async getText(selector: string): Promise<string | null> {
+    return await this.page.textContent(selector);
+  }
+
+  async isChecked(selector: string): Promise<boolean> {
+    return await this.page.isChecked(selector);
+  }
+}
---
+++ tests/testsuite01.spec.ts
@@ -0,0 +1,48 @@
+import { test, expect } from '@playwright/test';
+
+import { LoginPage } from './pages/loginPage.ts';
+import { DashboardPage } from './pages/dashboardPage.ts';
+import { CreateRoomPage } from './pages/createRoomPage.ts';
+
+
+test.describe('Test suite 01', () => {
+  test('Test case 01', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    const dashboardPage = new DashboardPage(page);
+
+    await loginPage.goto();
+    await loginPage.performLogin(`${process.env.TEST_USERNAME}`, `${process.env.TEST_PASSWORD}`)
+    await expect(page.getByRole('heading', { name: 'Tester Hotel Overview' })).toBeVisible();
+    await dashboardPage.performLogout();
+    await expect(page.getByRole('heading', { name: 'Login' })).toBeVisible();
+    await page.waitForTimeout(5000);
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+// test('has title', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
+
+//   // Expect a title "to contain" a substring.
+//   await expect(page).toHaveTitle(/Playwright/);
+// });
+
+// test('get started link', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
+
+//   // Click the get started link.
+//   await page.getByRole('link', { name: 'Get started' }).click();
+
+//   // Expects page to have a heading with the name of Installation.
+//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+// });
