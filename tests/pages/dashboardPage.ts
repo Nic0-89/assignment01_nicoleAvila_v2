fb6cca84da9ac2dfb774284bddff610ae446de36
Nicole Avila <avila.nicky89@gmail.com>
2024-09-12 18:23:02 +0200

    Finished Create room Page =)

--- tests/pages/dashboardPage.ts
+++ tests/pages/dashboardPage.ts
@@ -9,30 +9,32 @@ export class DashboardPage {
   readonly reservationButton: Locator;
   readonly logoutButton: Locator;
 
-//constructor
+  //constructor
   constructor(page: Page) {
     this.page = page;
-    this.roomButton= page.locator('div').filter({ hasText: /^RoomsNumber: 3View$/ }).getByRole('link') ;
-    this.clientButton= page.locator('div').filter({ hasText: /^ClientsNumber: 3View$/ }).getByRole('link');
-    this.billButton= page.locator('div').filter({ hasText: /^BillsTotal: 2 \(6500kr\)Paid: 1 \(2000kr\)View$/ }).getByRole('link')
-    this.reservationButton= page.locator('div').filter({ hasText: /^BillsTotal: 2 \(6500kr\)Paid: 1 \(2000kr\)View$/ }).getByRole('link');
-
-
+    this.roomButton = page.locator('a[href="/rooms"]');
+    this.clientButton = page.locator('a[href="/clients"]');
+    this.billButton = page.locator('a[href="/bills"]');
+    this.reservationButton = page.locator('a[href="/reservations"]');
 
     this.logoutButton = page.getByRole('button', { name: 'Logout' });
 
   }
 
- 
   async goToRooms() {
     await this.roomButton.click();
   }
-  
+  async goToClients() {
+    await this.clientButton.click();
+  }
+  async goToBills() {
+    await this.billButton.click();
+  }
+  async goToReservations() {
+    await this.reservationButton.click();
+  }
+
   async performLogout() {
     await this.logoutButton.click();
   }
 }
-
-
-
-// #app > div > div > div:nth-child(1) > a
